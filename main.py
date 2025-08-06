3d332aad5c15c0b84d43bffbc9c15cd7fd070965
brandonbullen <brandonrbullen@gmail.com>
2025-08-06 00:24:08 -0400

    Let's make ourselves a player

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pygame
 
 from constants import *
+from player import Player
 
 def main():
     # Initialize all pygame modules.
@@ -11,20 +12,31 @@ def main():
         (SCREEN_WIDTH, SCREEN_HEIGHT)
     )
 
+    # Create our game clock.
+    clock = pygame.time.Clock()
+    dt = 0
+
+    # Create our player.
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
     # Begin game loop
     while True:
-        # Set the background.
-        screen.fill('black')
-        
         # Exit logic
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-            
+        
+        # Set the background.
+        screen.fill('black')
+    
+        # Draw our player every frame.
+        player.draw(screen)
+
         # Refresh display
         pygame.display.flip()
-    
 
+        # limit framerate to 60 fps.
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
